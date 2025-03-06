a7e19d82a8c3f9baf9c4c7952e054940f315625b
SandyGit24 <sandrajacintha2023@gmail.com>
2025-03-06 23:24:53 +0000

    Added cors installation in config

--- fashion_project/settings.py
+++ fashion_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'fashion_api',
 
@@ -51,6 +52,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",  # Add your allowed origin with scheme
+    "https://127.0.0.1:8000",  # If you're using https
+    # Add any other origins you want to allow
 ]
 
 ROOT_URLCONF = 'fashion_project.urls'
